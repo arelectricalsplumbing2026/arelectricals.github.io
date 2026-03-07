fd55029fbbc96540995b82f84a4c7be6f3657f72
arelectricalsplumbing2026 <arelectricalsplumbing@gmail.com>
2026-03-07 17:04:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 console.log("AR Electricals Website Loaded");
+
+let members = document.querySelectorAll(".team-member");
+
+let index = 0;
+
+setInterval(function(){
+
+members[index].classList.remove("active");
+
+index++;
+
+if(index >= members.length){
+index = 0;
+}
+
+members[index].classList.add("active");
+
+},3000);
